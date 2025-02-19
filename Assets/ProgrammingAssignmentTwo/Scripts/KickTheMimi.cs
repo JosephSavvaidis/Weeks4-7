4b969c870aa24ead2f85b97d9d64da1ca2d8bd9c
Joseph Savvaidis <savvaidj@shernet.sheridancollege.ca>
2025-02-19 18:45:25 -0500

    Added new ways to attack the mimi charcater

--- Assets/ProgrammingAssignmentTwo/Scripts/KickTheMimi.cs
+++ Assets/ProgrammingAssignmentTwo/Scripts/KickTheMimi.cs
@@ -11,11 +11,17 @@ public class KickTheMimi : MonoBehaviour
     //get the gameobject for the healthbar
     public Image healthBar;
 
+    //checks when to play the finakl smoke animnation becaus etehre is some overlap with  the deathanimation
     bool playDeathSmoke;
+
+    public GameObject mimi;
     
     void Start()
     {
+        //setting playdeathsmoke to false at the beggining of the game
+        healthBar.fillAmount = 1;
         playDeathSmoke = false;
+        animator.SetInteger("Attack", 0);
     }
 
     // Update is called once per frame
@@ -28,7 +34,7 @@ public class KickTheMimi : MonoBehaviour
                 animator.SetInteger("Attack", 10);
                 //Debug.Log("Death by mimi");
             }
-            else if (playDeathSmoke == true) {
+            else if (healthBar.fillAmount <= 0 && playDeathSmoke == true) {
                 animator.SetInteger("Attack", 11);
             }
         }
@@ -36,19 +42,44 @@ public class KickTheMimi : MonoBehaviour
     //this piece of code sets the mimi's animation to attack 1 if he gets clicked by the player
     public void HitMimi() {
         animator.SetInteger("Attack", 1);
-        healthBar.fillAmount -= 0.1f;
         
+        
+    }
+    public void DoDamage() {
+        healthBar.fillAmount -= 0.1f;
     }
     //this peice of code resets the mimi's attack animation so that he goes back to idle
     public void ResetMimi()
     {
         animator.SetInteger("Attack", 0);
     }
+    public void ShootKill()
+    {
+        animator.SetInteger("Attack", 2);
+
+    }
+    public void SlashMimi() {
+        animator.SetInteger("Attack", 3);
+    }
+
+    //plays death smoke animation
+    
     public void deathMimi() {
         playDeathSmoke = true;
         animator.SetInteger("Attack", 11);
     }
-    public void destoryMIMI() { 
-    Destroy(this.gameObject);
+    //destroys the mimi when whole death process is complete
+    //creates new mimi whne the old one dies(instantiated from a prefab
+    public void NewMimi() { 
+    Instantiate(mimi);
+    }
+
+    public void bonkMimi() {
+    
+    }
+    public void destoryMIMI() {
+        
+        //animator.SetInteger("Attack", 0);
+        Destroy(this.gameObject);
     }
 }
